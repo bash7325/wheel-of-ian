32346f31947503f5434c4d4382cb824d1c509ad0
Brandon Ashby <bash7325@gmail.com>
2023-03-16 19:48:35 -0600

    trying to get categories on wheel and an arrow

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const result = document.getElementById('result');
 
 const categories = [];
 
-function drawWheel() {
+function drawWheel(rotation) {
   const ctx = wheelCanvas.getContext('2d');
   ctx.clearRect(0, 0, wheelCanvas.width, wheelCanvas.height);
 
@@ -17,7 +17,7 @@ function drawWheel() {
 
   categories.forEach((category, index) => {
     ctx.beginPath();
-    ctx.arc(centerX, centerY, radius, (index / categories.length) * Math.PI * 2, ((index + 1) / categories.length) * Math.PI * 2);
+    ctx.arc(centerX, centerY, radius, ((index / categories.length) * Math.PI * 2) + rotation, (((index + 1) / categories.length) * Math.PI * 2) + rotation);
     ctx.lineTo(centerX, centerY);
     ctx.closePath();
 
@@ -27,8 +27,28 @@ function drawWheel() {
 }
 
 function spinWheel() {
-  const randomIndex = Math.floor(Math.random() * categories.length);
-  result.textContent = `Selected Category: ${categories[randomIndex]}`;
+  const spinDuration = 5000; // Spin duration in milliseconds
+  const start = performance.now();
+  const initialRotation = Math.random() * Math.PI * 2;
+  const totalRotations = 5 + Math.floor(Math.random() * 5); // Spin at least 5 times and at most 10 times
+
+  function animate(now) {
+    const elapsed = now - start;
+    const progress = Math.min(elapsed / spinDuration, 1);
+    const easeOutCubic = (t) => (--t) * t * t + 1;
+    const currentRotation = initialRotation + easeOutCubic(progress) * totalRotations * Math.PI * 2;
+
+    drawWheel(currentRotation);
+
+    if (progress < 1) {
+      requestAnimationFrame(animate);
+    } else {
+      const selectedCategoryIndex = Math.floor((currentRotation / (Math.PI * 2)) * categories.length) % categories.length;
+      result.textContent = `Selected Category: ${categories[selectedCategoryIndex]}`;
+    }
+  }
+
+  requestAnimationFrame(animate);
 }
 
 categoryForm.addEventListener('submit', (event) => {
@@ -39,7 +59,7 @@ categoryForm.addEventListener('submit', (event) => {
     const listItem = document.createElement('li');
     listItem.textContent = category;
     categoryList.appendChild(listItem);
-    drawWheel();
+    drawWheel(0);
     spinButton.disabled = false;
   }
   categoryInput.value = '';
